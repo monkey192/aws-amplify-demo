c3f9959267ee046acba96d21273d094cba861ab1
monkey <thonq1@ITFJPs-MacBook-Pro.local>
2022-07-18 23:11:35 +0900

    added auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator } from "@aws-amplify/ui-react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <h1>We now have Auth!</h1>
         </p>
         <a
           className="App-link"
@@ -23,4 +23,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
